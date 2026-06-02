7c4995246f8ba3a6f979f73a5f45c997e593f0f9
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-06-02 23:22:43 +0800

    fix: game mode

--- scenes/game mode/gamemode.gd
+++ scenes/game mode/gamemode.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var enemy_scene = preload("res://scenes/ui/main_story_ui.tscn")
+var main_ui = preload("res://scenes/ui/main_story_ui.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,13 +13,13 @@ func _process(delta: float) -> void:
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
-	print("pressed")
 
 
 func _on_main_story_pressed() -> void:
-	print("pressed")
-	get_tree().change_scene_to_file("res://scenes/ui/main_story_ui.tscn")
+	$MainStoryUI.visible = true;
+	$VirtualMachineUI.visible = false;
 
 
 func _on_vm_mode_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/virtual_machine_ui.tscn")
+	$MainStoryUI.visible = false;
+	$VirtualMachineUI.visible = true;
