641b3306e0663b8c35b773ddf855f584081a37cf
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-06-02 22:34:49 +0800

    fix: ui

--- scenes/game mode/gamemode.gd
+++ scenes/game mode/gamemode.gd
@@ -10,21 +10,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
-
-
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
+	print("pressed")
 
 
 func _on_main_story_pressed() -> void:
-	pass # Replace with function body.
+	print("pressed")
+	get_tree().change_scene_to_file("res://scenes/ui/main_story_ui.tscn")
 
 
-func _on_virtual_machine_mode_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
+func _on_vm_mode_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/virtual_machine_ui.tscn")
