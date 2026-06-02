762ea933d3a028c070824503f780e0eff56c9494
klarencemy-cloud <klarencemy@gmail.com>
2026-06-02 18:11:42 +0800

    init
    fix sa start game

--- scenes/game mode/gamemode.gd
+++ scenes/game mode/gamemode.gd
@@ -10,6 +10,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
+
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
