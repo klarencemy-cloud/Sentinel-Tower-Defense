f2d39559ae66f0782ae10bef601258798428595a
klarencemy-cloud <klarencemy@gmail.com>
2026-06-02 16:19:51 +0800

    init
    UI - Main Menu, Game mode selection, Main story UI, and Virtual Machine UI

---
+++ scenes/game mode/gamemode.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var enemy_scene = preload("res://scenes/ui/main_story_ui.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
+
+
+func _on_main_story_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_virtual_machine_mode_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
---
+++ scenes/mainmenu/main_menu.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game mode/gamemode.tscn")
+
+
+func _on_sandbox_pressed() -> void:
+	pass
+
+
+func _on_database_pressed() -> void:
+	pass
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	pass
