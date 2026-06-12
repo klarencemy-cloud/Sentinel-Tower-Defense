d6e2316b5f5fbd99456f17e14ba87ea189a98e33
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-06-12 17:33:58 +0800

    feat: level2

--- scenes/managers/wave_manager.gd
+++ scenes/managers/wave_manager.gd
@@ -35,7 +35,7 @@ func start_wave() -> void:
 
 	Data.current_wave += 1
 
-	level_manager._change_current_map()
+	# level_manager._change_current_map()
 
 	if Data.current_wave % 5 == 0:
 		Data.checkpoint_wave = Data.current_wave
