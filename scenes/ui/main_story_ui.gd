7ecbf263c9e04e9da907697aaf7aca20ece450c8
klarencemy-cloud <klarencemy@gmail.com>
2026-06-14 18:41:20 +0800

    Main Story Wave count and Difficulty, working na

--- scenes/ui/main_story_ui.gd
+++ scenes/ui/main_story_ui.gd
@@ -1,14 +1,24 @@
 extends Node
 
+@onready var wave: Label = $Text/Wave
+@onready var difficulty: Label = $Text/Difficulty
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	wave.text = ("Wave " + str(Data.current_wave))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if Data.current_wave > 0 and Data.current_wave < 20:
+		difficulty.text = ("Easy")
+		difficulty.add_theme_color_override("font_color", Color.GREEN)
+	elif Data.current_wave > 19 and Data.current_wave < 30:
+		difficulty.text = ("MEDIUM")
+		difficulty.add_theme_color_override("font_color", Color.ORANGE)
+	elif Data.current_wave > 29 and Data.current_wave < 50:
+		difficulty.text = ("HARD")
+		difficulty.add_theme_color_override("font_color", Color.RED)
+	else:
+		difficulty.text = ("EXTREME")
+		difficulty.add_theme_color_override("font_color", Color(0.5, 0, 0))
 
 
 func _on_start_game_pressed() -> void:
