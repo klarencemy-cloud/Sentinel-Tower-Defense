bbd5e8f5578b6096f9baf7ba1a30539c5620ca94
allendimal123 <dimalclydeallen@gmail.com>
2026-07-21 21:01:00 +0800

    feat: homing bullets

--- scenes/towers/tower_spamfilter.gd
+++ scenes/towers/tower_spamfilter.gd
@@ -30,7 +30,8 @@ func _on_reload_timer_timeout() -> void:
 			bullet_type,
 			final_damage,
 			type,
-			tower_id
+			tower_id,
+			enemies[0]   # <-- target
 		)
 		fire_animation()
 		$ShootSound.play()
