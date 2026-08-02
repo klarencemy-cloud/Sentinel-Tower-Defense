5ea85067c617e8b22f452d89bdc6f6b0563fa57c
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-08-02 10:13:06 +0800

    fix: server collision

---
+++ scenes/server/server_collision.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.get_overlapping_areas().is_empty():
+		Data.is_tower_placeable = true
+
+func _on_area_entered(area: Area2D) -> void:
+	Data.is_tower_placeable = false
