627b7947bc310133077e33c0b7a2db0d3ebf55d6
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-09-15 20:36:34 +0800

    Update sentinel_database_ui.gd

--- scenes/ui/sentinel_database_ui.gd
+++ scenes/ui/sentinel_database_ui.gd
@@ -61,35 +61,35 @@ var SENTINEL_DATA = {
 		'cooldown': "1 minute and 30 seconds",
 		'passive_ability': "Generates gold and EXP periodically for the player.",
 		'irl_desc': "Their main role is to provide support, troubleshoot problems, and ensure that the computer infrastructure, such as servers and the network, is functioning.",
-		'thumbnail':"res://graphics/sentinels/thumbnail/SYSTEMADMIN.png"
+		'thumbnail': "res://graphics/sentinels/thumbnail/SYSTEMADMIN.png"
 	},
 		Sentinel.INTRUSION: {
 		'special_ability': "Deploys a shield with 1500 hit points around the Server that reflects damage to attackers.",
 		'cooldown': "30 seconds",
 		'passive_ability': "Reduce damage to the server by 5%.",
 		'irl_desc': "An Intrusion Analyst is responsible for detecting, analyzing, and responding to cybersecurity threats or unauthorized access within an organization's computer networks. They monitor network traffic, investigate security incidents, and use specialized tools to identify potential breaches or vulnerabilities. Their work helps prevent data loss and protects sensitive information by quickly addressing and mitigating cyber threats. Additionally, they often collaborate with other IT and security teams to improve overall security posture and may assist in developing security policies and response plans.",
-		'thumbnail':"res://graphics/sentinels/thumbnail/INTRUSIONANALYST.png"
+		'thumbnail': "res://graphics/sentinels/thumbnail/INTRUSIONANALYST.png"
 	},
 		Sentinel.SECURITY: {
 		'special_ability': "Increases nearby towers' attack speed by 15% for 10 seconds.",
 		'cooldown': "15 seconds",
 		'passive_ability': "Nearby towers gain an additional 25% range. ",
 		'irl_desc': "Security Architects design, develop, and implement systems that prevent the infiltration of malware and other hacker-related intrusions across the IT network, thereby helping organisations to continue their activities without encouraging costly and damaging situations.",
-		'thumbnail':"res://graphics/sentinels/thumbnail/SECURITYARCHITECT.png"
+		'thumbnail': "res://graphics/sentinels/thumbnail/SECURITYARCHITECT.png"
 	},
 		Sentinel.MALWARE: {
 		'special_ability': "Examines detected threats, reveals their weaknesses, and instead of directly attacking enemies, it improves the effectiveness of other nearby defense towers' damage by 30% for 15 seconds.",
 		'cooldown': "25 seconds",
 		'passive_ability': "Nearby towers gain an additional 10% crit chance.  ",
 		'irl_desc': "A malware analyst examines malicious files and applications to comprehend how malware operates and how it can be prevented or countered. Their perspectives assist cybersecurity teams in identifying, examining, and protecting against cyber threats. They provide information on malicious software, revealing its function, what it aims for, and how actors utilize it. Additionally, they are also combating malicious software.",
-		'thumbnail':"res://graphics/sentinels/thumbnail/MALWAREANALYST.png"
+		'thumbnail': "res://graphics/sentinels/thumbnail/MALWAREANALYST.png"
 	},
 		Sentinel.DECEPTION: {
 		'special_ability': "Disorient enemies upon approaching the Server for 15 seconds, causing enemies near the Server to change direction.",
 		'cooldown': "30 seconds",
 		'passive_ability': "Reduce damage to the server by 5%.",
 		'irl_desc': "The Deception Specialist handles deception technology,  which is a strategy to attract cyber criminals away from an enterprise's true assets and divert them to a decoy or trap. The decoy mimics legitimate servers, applications, and data so that the criminal is tricked into believing that they have infiltrated and gained access to the enterprise's most important assets when in reality they have not. The strategy is employed to minimize damage and protect an organization's true assets.",
-		'thumbnail':"res://graphics/sentinels/thumbnail/DECEPTIONANALYST.png"
+		'thumbnail': "res://graphics/sentinels/thumbnail/DECEPTIONANALYST.png"
 	},
 }
 
@@ -105,10 +105,10 @@ func _on_sentinel_6_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
@@ -136,10 +136,10 @@ func _on_sentinel_5_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
@@ -167,10 +167,10 @@ func _on_sentinel_4_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
@@ -198,10 +198,10 @@ func _on_sentinel_3_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
@@ -230,10 +230,10 @@ func _on_sentinel_2_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
@@ -262,10 +262,10 @@ func _on_sentinel_1_pressed() -> void:
 		UISound.play_click()
 
 		desc_name.text = "???"
-		desc_sp.text = "Ability: ???"
-		desc_cd.text = "Cooldown: ???"
-		desc_passive.text = "Passive: ???"
-		desc_desc.text = "???"
+		desc_sp.text = "Ability: Unknown"
+		desc_cd.text = "Cooldown: Unknown"
+		desc_passive.text = "Passive: Unknown"
+		desc_desc.text = "No Available Data"
 
 		$Databasebg/AnimatedSprite2D.visible = false
 		$Databasebg/BigPic.visible = true
