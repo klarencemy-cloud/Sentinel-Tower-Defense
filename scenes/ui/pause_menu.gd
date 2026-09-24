1d05c4729a8824a70ecd4adf05617e5795af0c24
HuynhKent14 <huynhdeleonkent@gmail.com>
2026-09-24 09:44:54 +0800

    fix: ending dialogue and boss 6 scale

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false # makes sure it disappears at startup, just like my money
+	GameDialogueManager.quit_game.connect(_on_quit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
